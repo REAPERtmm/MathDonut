0a3a9522c6d5f191d7132ae745b8ebe8e40ce752
xxxTristantacion <96882843+REAPERtmm@users.noreply.github.com>
2025-11-04 09:08:36 +0100

    Fin de séance

--- Math.cpp
+++ Math.cpp
@@ -15,10 +15,29 @@ int main(int argc, char* argv[])
 	DWORD mode;
 	GetConsoleMode(hConsole, &mode);
 	SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-	
-	std::cout << HIDE_CUR << RESET;
 
-	std::cout << "Hello World" << std::endl;
+	COORD max = GetLargestConsoleWindowSize(hConsole);
+	SHORT w = (100 > max.X) ? max.X : 100;
+	SHORT h = (20 > max.Y) ? max.Y : 20;
+
+	SMALL_RECT win = { 0, 0, (SHORT)(w - 1), (SHORT)(h - 1) };
+	// Enlarge buffer first (for growth), then set window.
+	SetConsoleScreenBufferSize(hConsole, { w, h });
+	SetConsoleWindowInfo(hConsole, TRUE, &win);
+
+	std::cout << HIDE_CUR << CLEAR;
+
+	char buffer[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int index = 0;
+
+	for (int i = 0; i < 100; i++)
+	{
+		for (int j = 0; j < 21; j++)
+		{
+			std::cout << buffer[index];
+			index = (++index) % 27;
+		}
+	}
 
 	std::cout << SHOW_CUR;
 
