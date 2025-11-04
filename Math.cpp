9e9bd0074d38351d151ac0abc7a1f6d2074af481
xxxTristantacion <96882843+REAPERtmm@users.noreply.github.com>
2025-11-04 08:41:52 +0100

    [EXO] 1.2

--- Math.cpp
+++ Math.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#define CURSOR_START "\x1B[H"
+#define CLEAR "\x1B[2J"
+#define DEFAULT "\x1B[0m"
+#define RED "\x1B[38;2;255;0;0m"
+#define RESET CLEAR << CURSOR_START
+#define HIDE_CUR "\x1B[?25l"
+#define SHOW_CUR "\x1B[?25h"
+
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello World" << std::endl;
-
+	std::cout << HIDE_CUR << "IDnqibndiqzb" << RESET << "dnqzdqz" << DEFAULT << std::endl;
 	return 0;
 }
 
